7d94f3d754101e02f5cf6c31effef1d04b2fe02d
PhiriMichael <chikaonda06@gmail.com>
2025-03-16 14:44:36 +0200

    push

--- index.php
+++ index.php
@@ -122,8 +122,6 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2025 <a href="https://www.gusherlabs.com/">Gusherlabs.com</a>.</strong>
-    All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b><?php echo $_SESSION['system']['name'] ?></b>
     </div>
